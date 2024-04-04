9b3548c1d8912a5f86f074398440c2bfd5face83
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-04-03 19:24:31 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Assign1_WCram
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
